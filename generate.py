895ed51cad553fdb28dd6a5768d4293fbb2fa7fd
GorbunovaAA <gorbunova.aa@phystech.edu>
2018-04-05 19:00:08 +0300

    Добавлены классы и исключения

--- generate.py
+++ generate.py
@@ -2,88 +2,126 @@ import life
 
 
 def read_data():
-	print("Choose the type of reading your data:")
-	print('\'1 input\' to read from std input')
-	print('\'2 filename\' to read from file')
 
-	s_input = input().split(' ')
-	assert s_input[0] in ['1', '2'] and len(s_input) == 2, 'incorrect input'
-	lines = []
-	if s_input[0] == '1':
-		assert s_input[1] == 'input', 'incorrect input'
-		print("input osean_sizes and generation_count")
-		m, n, n_gen = map(int, input().split(' '))
-		for _ in range(n):
-			lines.append(input())
+    while(True):
+        all_fine = 1
+        print("Choose the type of reading your data:")
+        print('\'1 input\' to read from std input')
+        print('\'2 filename\' to read from file')
+        s_input = input().split(' ')
+        lines = []
+        try:
+            if s_input[0] == '1':
+                print("input osean_sizes and generation_count")
+                try:
+                    n, m, n_gen = map(int, input().split(' '))
+                except ValueError:
+                    all_fine = 0
+                    print('incorrect input: not enough values\n')
+                    continue
 
-	if s_input[0] == '2':
-		with open(s_input[1], 'r') as f:
-			m, n, n_gen = map(int, f.readline().split(' '))
-			for line in f:
-				lines.append(line.replace('\n', ''))
-	# print(lines)
-	return m, n, n_gen, lines
+                for _ in range(n):
+                    try:
+                        lines.append(input())
+                        cells = lines[-1].split(' ')
+                        for i in range(m):
+                            cell = cells[i]
+                            assert cell in ('empty', 'stone', 'fish', 'shrimp')
+                    except IndexError:
+                        all_fine = 0
+                        print('incorrect input: not enough values\n')
+                        break
+                    except AssertionError:
+                        all_fine = 0
+                        print('incorrect input: unknown type of cell\n')
+                        break
+
+            if s_input[0] == '2':
+                filename = s_input[1]
+                print("reading from file {}".format(filename))
+                n, m, n_gen = 0, 0, 0
+                try:
+                    f = open(filename, 'r')
+                    n, m, n_gen = map(int, f.readline().split(' '))
+                except FileNotFoundError:
+                    print('file {} not found\n'.format(filename))
+
+                except ValueError:
+                    all_fine = 0
+                    print('incorrect input: not enough values\n')
+                    continue
+
+                finally:
+                    for _ in range(n):
+                        try:
+                            lines.append(f.readline().replace('\n', ''))
+                            cells = lines[-1].split(' ')
+                            for i in range(m):
+                                cell = cells[i]
+                                assert cell in ('empty', 'stone', 'fish', 'shrimp')
+                        except IndexError:
+                            all_fine = 0
+                            print('incorrect input: not enough cells in row\n')
+                            break
+                        except AssertionError:
+                            all_fine = 0
+                            print('incorrect input: unknown type of cell\n')
+                            break
+                    f.close()
+                        
+            elif s_input[0] == '2':
+                with open(filename, 'r') as f:
+                    n, m, n_gen = map(int, f.readline().split(' '))
+                    for line in f:
+                        lines.append(line.replace('\n', ''))
+            # print(lines)
+        except IndexError:
+            all_fine = 0
+            print('incorrect input: lack of arguments\n')
+            continue
+
+        if all_fine:
+            break
+    
+    return m, n, n_gen, lines
+
 
 # read_data()
 def write_data(ocean):
-	print("Choose the type of writing your data:")
-	print('\'1 output\' to write to std output')
-	print('\'2 filename\' to write to file')
+    print("Choose the type of writing your data:")
+    print('\'1 output\' to write to std output')
+    print('\'2 filename\' to write to file')
 
-	s_output = input().split(' ')
-	assert s_output[0] in ['1', '2'] and len(s_output) == 2, 'incorrect input'
-	if s_output[0] == '1':
-		assert s_output[1] == 'output', 'incorrect input'
-		# print('current generation = {}'.format(gen))
-		for line in ocean:
-			for cell in line:
-				print(cell, end=' ')
-			print('')
+    s_output = input().split(' ')
+    assert s_output[0] in ('1', '2') and len(s_output) == 2, 'incorrect input'
+    if s_output[0] == '1':
+        assert s_output[1] == 'output', 'incorrect input'
+        # print('current generation = {}'.format(gen))
+        for line in ocean:
+            for cell in line:
+                print(cell, end=' ')
+            print('')
 
-	if s_output[0] == '2':
-		with open(s_output[1], 'w+') as f:
-			# f.write('current generation = {}'.format(gen))
-			# f.write('\n')
-			for line in ocean:
-				for i, cell in enumerate(line):
-					f.write(cell)
-					if i < len(line):
-						f.write(' ')
-				f.write('\n')
+    if s_output[0] == '2':
+        with open(s_output[1], 'w+') as f:
+            # f.write('current generation = {}'.format(gen))
+            # f.write('\n')
+            for line in ocean:
+                for i, cell in enumerate(line):
+                    f.write(cell)
+                    if i < len(line):
+                        f.write(' ')
+                f.write('\n')
 
-
-# m, n, n_gen, lines = read_data()
-# ocean = [[] for i in range(n)]
-# for i in range(n):
-# 	ocean[i] = list(map(str, lines[i].split()))
-# print(ocean)
-# write_data(ocean)
-
-
-
-# with open("input.txt", 'r') as f:
-# 	for line in f:
-# 		pass
-# 	print(line[-1])
-# 	print(s0)
-# 	print(str(s1))
-# print(s1)
-# print(len(s1.split('\n')))
-# if(input() == "1"):
-# 	f1 = input()
-# else:
-# 	print("Choose the name of input file")
-# 	with open(input(), "r") as f1:
 lines = []
 n, m, n_gen, lines = read_data()
 ocean = [[] for i in range(n)]
 for i in range(n):
-	ocean[i] = list(lines[i].split())
-	assert len(ocean[i]) == m, 'incorrect input, try again'
+    ocean[i] = list(lines[i].split())
 
 for i in range(n_gen):
-	ocean = life.next_gen(ocean);
-	# print('current generation = {}'.format(n_gen))
-	# for j in range(n):
-		# print(*ocean[j])
+    ocean = life.next_gen(ocean)
+    # print('current generation = {}'.format(n_gen))
+    # for j in range(n):
+    #     print(*ocean[j])
 write_data(ocean)
--- life.py
+++ life.py
@@ -1,56 +1,84 @@
-def get_neib(ocean, i, j):
-	n = len(ocean)
-	m = len(ocean[0])
-	neib = {'fish': 0, 'shrimp': 0, 'stone': 0, 'empty': 0}
-	def add_neib(value):
-		neib[value] = neib.get(value, 0) + 1;
-	if(i > 0):
-		if(j > 0):
-			add_neib(ocean[i-1][j-1])
-		if(j < m - 1):
-			add_neib(ocean[i-1][j+1])
-		add_neib(ocean[i-1][j])
-	if(i < n - 1):
-		if(j > 0):
-			add_neib(ocean[i+1][j-1])
-		if(j < m - 1):
-			add_neib(ocean[i+1][j+1])
-		add_neib(ocean[i+1][j])
-	if(j > 0):
-		add_neib(ocean[i][j-1])
-	if(j < m - 1):
-		add_neib(ocean[i][j+1])
-	return neib
+class Cell():
+    type_ = 'cell'
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.is_alive = False
+    
+class Empty(Cell):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+        self.type_ = "empty"
+    def get_new_cell_type(self, neib):
+        for animal in animals:
+            if neib[animal] == 3:
+                return animal
+        return self.type_
 
-def new_creature(x, neighbors):
-	# print(neighbors)
-	if(x == 'fish'):
-		if (neighbors['fish']) <= 1 or (neighbors['fish']) >= 5:
-			return "empty"
-		return "fish"
-	if(x == 'shrimp'):
-		if (neighbors['shrimp']) <= 1 or (neighbors['shrimp']) >= 5:
-			return "empty"
-		return "shrimp"
-	if(x == 'empty'): 
-		if neighbors['fish'] == 3:
-			return 'fish'
-		if neighbors['shrimp'] == 3:
-			return 'shrimp'
-		return 'empty'
-	return 'stone'
+class Stone(Cell):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+        self.type_ = "stone"
+    def get_new_cell_type(self, neib):
+        return self.type_
+
+
+class Animal(Cell):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+        self.is_alive = True
+        self.type_ = "animal"
+    def get_new_cell_type(self, neib):
+        if (neib[self.type_]) <= 1 or (neib[self.type_]) >= 5:
+            return "empty"
+        return self.type_
+
+class Fish(Animal):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+        self.type_ = "fish"
+
+class Shrimp(Animal):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+        self.type_ = "shrimp"
+
+sell_by_type = {"cell": Cell, "empty": Empty, "stone": Stone, "animal": Animal, "fish": Fish, "shrimp": Shrimp}
+animals = ['fish', 'shrimp']
+
+
+def get_neib(cell, ocean):
+    n = len(ocean)
+    m = len(ocean[0])
+    neib = {'fish': 0, 'shrimp': 0, 'stone': 0, 'empty': 0}
+    def add_neib(value):
+        neib[value] = neib.get(value, 0) + 1;
+
+    for shift_x in (-1, 0, 1):
+        for shift_y in (-1, 0, 1):
+            if 2 * shift_x + shift_y != 0:
+                if 0 <= cell.x + shift_x < n and 0 <= cell.y + shift_y < m:
+                    add_neib(ocean[cell.x + shift_x][cell.y + shift_y])
+    return neib
+
+def new_creature(cell, ocean):
+    neib = get_neib(cell, ocean)
+    print(cell.type_, neib, cell.get_new_cell_type(neib))
+    return cell.get_new_cell_type(neib)
 
 def next_gen(ocean):
-	n = len(ocean)
-	m = len(ocean[0])
-	new_ocean = [[''] * m for i in range(n)]
-	for i in range(n):
-		for j in range(m):
-			neighbors = get_neib(ocean, i, j)
-			# print(i, j, 'Neighbors:', *neighbors)
-			new_ocean[i][j] = new_creature(ocean[i][j], neighbors)
-			# for k in range(n):
-				# print(*new_ocean[i])
-	return new_ocean
+    n = len(ocean)
+    m = len(ocean[0])
+    new_ocean = [[''] * m for i in range(n)]
+
+    cell_ocean = [[[] for j in range(m) ]  for i in range(n)]
+    for i in range(n):
+        for j in range(m):
+            cell_ocean[i][j] = sell_by_type[ocean[i][j]](i, j)
+
+    for i in range(n):
+        for j in range(m):
+            new_ocean[i][j] = new_creature(cell_ocean[i][j], ocean)
+    return new_ocean
 
 
